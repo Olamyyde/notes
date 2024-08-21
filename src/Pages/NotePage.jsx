71aa38058c79a87014867e08cb07873b8d81655b
Olamide Adebimpe <olamide369@gmail.com>
2024-08-20 21:15:05 -0400

    component for note card

--- src/Pages/NotePage.jsx
+++ src/Pages/NotePage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Notecard from "../components/Notecard";
+import {fakeData as notes} from "../assets/fakeData";
 
 const NotesPage = () => {
-    return <div>NotesPage</div>
+    return (
+    <div>
+        {notes.map(note => (
+            <Notecard key={note.$id} note={note}/>
+        ))}
+    </div>
+    )
 }
 
 export default NotesPage;
